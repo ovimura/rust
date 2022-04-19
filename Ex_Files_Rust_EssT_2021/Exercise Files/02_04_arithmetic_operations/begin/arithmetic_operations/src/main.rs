9e51f088037d29ebb97bb429dd56bc36eb7a3907
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-04-19 08:41:57 -0700

    adding exercices for ch 2, section 4

--- Ex_Files_Rust_EssT_2021/Exercise Files/02_04_arithmetic_operations/begin/arithmetic_operations/src/main.rs
+++ Ex_Files_Rust_EssT_2021/Exercise Files/02_04_arithmetic_operations/begin/arithmetic_operations/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let a = 10;
-    let b = 3;
-    let c = a + b;
-    println!("c is {}", c);
+    let b = 3.0;
+    let c = a as f64 / (b);
+    print!("c is {0:08.3}\na is {1}\nonce again, c is {0}", c, a);
 }
