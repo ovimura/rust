b8677eb8542150154ebfd210caefb1c440445111
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-04-19 12:50:17 -0700

    adding exercices for ch 02, section 06

--- Ex_Files_Rust_EssT_2021/Exercise Files/02_06_bitwise_operations/begin/bitwise_operations/src/main.rs
+++ Ex_Files_Rust_EssT_2021/Exercise Files/02_06_bitwise_operations/begin/bitwise_operations/src/main.rs
@@ -1,4 +1,24 @@
 fn main() {
-    let value = 0b11110101;
+    let mut value = 0b1111_0101u8;
     println!("value is {}", value);
+    println!("value is {:08b}", value);
+
+    value = !value;
+    println!("value is {:08b}", value);
+
+    value = value & 0b1111_0111;
+    println!("value is {:08b}", value);
+    println!("bit 6 is {}", value & 0b0100_0000);
+
+    println!("value is {:08b}", value);
+    println!("bit 6 is {:08b}", value | 0b0100_0000);
+
+    value = value ^ 0b0101_0101;
+    println!("value is {:08b}", value);
+
+    value = value << 4;
+    println!("value is {:08b}", value);
+
+    value = value >> 2;
+    println!("value is {:08b}", value);
 }
