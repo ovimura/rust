7770916a61c7298934c4610ed7b7d4b05105f8cd
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-04-19 18:27:42 -0700

    adding exercices

--- Ex_Files_Rust_EssT_2021/Exercise Files/02_07_boolean_data/begin/boolean_data/src/main.rs
+++ Ex_Files_Rust_EssT_2021/Exercise Files/02_07_boolean_data/begin/boolean_data/src/main.rs
@@ -6,4 +6,10 @@ fn main() {
     println!("a AND b is {}", a & b);
     println!("a OR b is {}", a | b);
     println!("a XOR b {}", a ^ b);
+
+    let c = (a ^ b) || (a && b);
+    println!("c is {}", c);
+
+    let c = (a ^ b) && panic!();
+    println!("c is {}", c);
 }
--- Ex_Files_Rust_EssT_2021/Exercise Files/02_09_char_data/begin/char_data/src/main.rs
+++ Ex_Files_Rust_EssT_2021/Exercise Files/02_09_char_data/begin/char_data/src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
     let letter = 'a';
     let number = '1';
+    let finger = '\u{261D}';
+    println!(" {}\n {}\n {}\n", letter, number, finger);
 }
