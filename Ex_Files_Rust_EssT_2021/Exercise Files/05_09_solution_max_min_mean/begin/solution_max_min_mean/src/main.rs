2f8e67adcfcd1859f7a516a68fa8b9b82e3689f2
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-04-22 14:49:18 -0700

    adding exercices for chapter 05, section 09

--- Ex_Files_Rust_EssT_2021/Exercise Files/05_09_solution_max_min_mean/begin/solution_max_min_mean/src/main.rs
+++ Ex_Files_Rust_EssT_2021/Exercise Files/05_09_solution_max_min_mean/begin/solution_max_min_mean/src/main.rs
@@ -4,8 +4,20 @@ fn main() {
     let mut min: i32;
     let mut mean: f64;
 
-    /* YOUR CODE GOES HERE */
+    max = numbers[0];
+    min = numbers[0];
+    mean = 0.0;
 
+    for &item in numbers.iter() {
+        if item > max {
+            max = item;
+        } else if item < min {
+            min = item;
+        }
+        mean += item as f64;
+    }
+
+    mean /= numbers.len() as f64;
     assert_eq!(max, 56);
     assert_eq!(min, -18);
     assert_eq!(mean, 12.5);
