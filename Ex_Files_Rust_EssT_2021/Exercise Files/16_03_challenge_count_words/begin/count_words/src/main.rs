91edb7e52517e1a393c6bfaa392480781e82453e
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-04-29 10:05:17 -0700

    adding initial requirement and setting up the package for the last challenge

---
+++ Ex_Files_Rust_EssT_2021/Exercise Files/16_03_challenge_count_words/begin/count_words/src/main.rs
@@ -0,0 +1,9 @@
+/**
+ * 1. Read in a text file.
+ * 2. Count the number of time each word occurs.
+ * 3. Print a message with the most common words and how many times they appeared.
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
