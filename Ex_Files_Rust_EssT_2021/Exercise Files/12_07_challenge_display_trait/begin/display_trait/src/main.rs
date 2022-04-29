92e080e6e4d7bf44bc378c5e5176c217c4808079
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-04-29 09:30:32 -0700

    adding exercices solution for chapter 12, section 07

--- Ex_Files_Rust_EssT_2021/Exercise Files/12_07_challenge_display_trait/begin/display_trait/src/main.rs
+++ Ex_Files_Rust_EssT_2021/Exercise Files/12_07_challenge_display_trait/begin/display_trait/src/main.rs
@@ -1,14 +1,24 @@
+use std::fmt;
+
 struct Satellite {
     name: String,
-    velocity: f64 // miles per second
+    velocity: f64, // miles per second
 }
 
-/* YOUR CODE GOES HERE */
+impl fmt::Display for Satellite {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{} with a velocity of {} miles per second",
+            self.name, self.velocity
+        )
+    }
+}
 
 fn main() {
     let hubble = Satellite {
         name: String::from("Hubble Telescope"),
-        velocity: 4.72
+        velocity: 4.72,
     };
     println!("hubble is {}", hubble);
 }
