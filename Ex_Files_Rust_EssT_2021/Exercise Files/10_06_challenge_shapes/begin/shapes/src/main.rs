d30130f17a8069fa3412ff24a443a1eb9d94771b
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-04-29 08:56:44 -0700

    adding Chapter 10, section 06

--- Ex_Files_Rust_EssT_2021/Exercise Files/10_06_challenge_shapes/begin/shapes/src/main.rs
+++ Ex_Files_Rust_EssT_2021/Exercise Files/10_06_challenge_shapes/begin/shapes/src/main.rs
@@ -1,4 +1,28 @@
-/* YOUR CODE GOES HERE */
+struct Rectangle {
+    w: f64,
+    h: f64,
+}
+
+trait Shape {
+    fn new(a: f64, b: f64) -> Rectangle;
+    fn get_area(&self) -> f64;
+    fn scale(&mut self, a: f64);
+}
+
+impl Shape for Rectangle {
+    fn new(a: f64, b: f64) -> Rectangle {
+        return Rectangle { w: a, h: b };
+    }
+
+    fn scale(&mut self, a: f64) {
+        self.w *= a;
+        self.h *= a;
+    }
+
+    fn get_area(&self) -> f64 {
+        self.h * self.w
+    }
+}
 
 fn main() {
     let mut rect = Rectangle::new(1.2, 3.4);
