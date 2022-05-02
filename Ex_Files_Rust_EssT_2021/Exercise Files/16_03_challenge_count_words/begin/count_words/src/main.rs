c02174dc922d4020c379341be9d9f9b90758cdf0
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-05-02 14:43:07 -0700

    adding solution for chapter 16, section 04

--- Ex_Files_Rust_EssT_2021/Exercise Files/16_03_challenge_count_words/begin/count_words/src/main.rs
+++ Ex_Files_Rust_EssT_2021/Exercise Files/16_03_challenge_count_words/begin/count_words/src/main.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+use std::env;
+use std::fs;
+
 /**
  * 1. Read in a text file.
  * 2. Count the number of time each word occurs.
@@ -5,5 +9,32 @@
  */
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let mut map = HashMap::new();
+    if args.len() == 2 {
+        let file_name = &args[1];
+        let content = fs::read_to_string(file_name).expect("error: file cannot be opened");
+        let lines = content.split("\n");
+        for l in lines {
+            for w in l.split_whitespace() {
+                if map.get(w) == None {
+                    map.insert(w, 1);
+                } else {
+                    let v: i32 = map.get(w).unwrap() + 1;
+                    map.insert(w, v);
+                }
+            }
+        }
+        let mut maximum = 0;
+        let mut val = "";
+        for k in map.keys() {
+            if maximum < *map.get(k).unwrap() {
+                maximum = *map.get(k).unwrap();
+                val = k;
+            }
+        }
+        println!("The maximum (word, count): ({}, {})", val, maximum);
+    } else {
+        println!("usage: count_words.exe <file_name>");
+    }
 }
